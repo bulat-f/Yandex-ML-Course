96dfa5166c26e6182c971ccfc32282b31f7196de
fatbulat <fatbulat5@gmail.com>
2016-09-18 14:53:09 +0300

    Start to solve first week tasks

---
+++ week 1/task_1.py
@@ -0,0 +1,33 @@
+import pandas
+
+
+def task_1_1(data, out):
+    m_count = data.Sex[data.Sex == 'male'].count()
+    f_count = data.Sex[data.Sex == 'female'].count()
+    out.write(str(m_count) + ' ' + str(f_count))
+
+def task_1_2(data, out):
+    survived = data.Survived[data.Survived].count() / float(data.Survived.count()) * 100
+    out.write('%.2f' % survived)
+
+
+def task_1_3(data, out):
+    class_a = data.Cabin.dropna().count() / float(data.Survived.count())
+    out.write('%.2f' % class_a)
+
+def task_1_4(data, out):
+    out.write('%.2f' % data.Age.mean())
+    out.write(' ')
+    out.write('%.2f' % data.Age.median())
+
+def main():
+    data = pandas.read_csv('titanic.csv', index_col = 'PassengerId')
+    for i in range(1, 5):
+        name = 'task_1_' + str(i)
+        out = file(name + '.out', 'w')
+        method = globals().get(name)
+        method(data, out)
+        out.close()
+
+if __name__ == '__main__':
+    main()
